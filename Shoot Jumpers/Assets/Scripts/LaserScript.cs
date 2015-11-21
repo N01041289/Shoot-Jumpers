e2b564b2d1743c5944bcca7fa4cfd6d20fa63c9f
Oskold <Fletcher_gray@hotmail.com>
2015-11-20 23:23:26 -0500

    Learning Basic Skills
    So i was catching up on the basics this week but i added a laser that
    knocks around objects

---
+++ Shoot Jumpers/Assets/Scripts/LaserScript.cs
@@ -0,0 +1,68 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LaserScript : MonoBehaviour {
+
+    LineRenderer line;
+    public int laserDamage = 50;
+    bool cursorShouldBeLocked = true;
+
+    // Use this for initialization
+    void Start ()
+    {
+        Debug.Log("Lazer Script Online");
+        line = gameObject.GetComponent<LineRenderer>();
+        line.enabled = false;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (cursorShouldBeLocked)
+        {
+            Cursor.lockState = CursorLockMode.Locked;
+            Cursor.visible = false;
+        }
+        else
+        {
+            Cursor.lockState = CursorLockMode.None;
+            Cursor.visible = true;
+        }
+
+        if (Input.GetButtonDown("Fire1"))
+        {
+            StopCoroutine("FireLaser");
+            StartCoroutine("FireLaser");
+        }
+	}
+    IEnumerator FireLaser()
+    {
+        line.enabled = true;
+
+        while (Input.GetButton("Fire1"))
+        {
+            line.material.mainTextureOffset = new Vector2(0, Time.time);
+
+            Ray ray = new Ray(transform.position, transform.forward);
+            RaycastHit hit;
+
+            line.SetPosition(0, ray.origin);
+
+            if (Physics.Raycast(ray, out hit, 100))
+            {
+                line.SetPosition(1, hit.point);
+                if (hit.rigidbody)
+                {
+                    hit.rigidbody.AddForceAtPosition(transform.forward * laserDamage, hit.point);
+                }
+            }
+            else
+            { 
+                line.SetPosition(1, ray.GetPoint(100));
+            }
+            yield return null;
+        }
+
+        line.enabled = false;
+    }
+}
---
+++ Shoot Jumpers/Assets/Scripts/TestScript.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TestScript : MonoBehaviour {
+
+    public float speed = 20;
+    public Rigidbody projectile;
+
+    // Use this for initialization
+    void Start()
+    {
+        Debug.Log("Test Script Online");
+    }
+
+    // Update is called once per frame
+    void Update ()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Debug.Log("Firing");
+
+            Rigidbody instanciateProjectile = Instantiate(projectile, transform.position, transform.rotation) as Rigidbody;
+            instanciateProjectile.velocity = transform.TransformDirection(new Vector3(0,0,speed));
+        }
+    }
+}
